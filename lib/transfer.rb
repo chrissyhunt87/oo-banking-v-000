7e56f7f7d8164672561c82365c0e05b7ff6521e6
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-27 12:22:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :last_sender, :last_receiver, :last_amount
 
   def initialize(sender, receiver, amount)
     @sender = sender
